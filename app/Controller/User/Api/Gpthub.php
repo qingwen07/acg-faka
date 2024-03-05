8fc957606d2fc7186f83138267f3787fb84171f5
xqw <xiongqingwen@mycs.cn>
2024-03-05 11:47:23 +0800

    modify Gpthub.php

--- app/Controller/User/Api/Gpthub.php
+++ app/Controller/User/Api/Gpthub.php
@@ -5,6 +5,13 @@ namespace App\Controller\User\Api;
 
 
 use App\Controller\Base\API\User;
+use App\Interceptor\UserVisitor;
+use App\Interceptor\Waf;
+use Kernel\Annotation\Get;
+use Kernel\Annotation\Inject;
+use Kernel\Annotation\Interceptor;
+use Kernel\Annotation\Post;
+use Kernel\Exception\JSONException;
 
 #[Interceptor([Waf::class, UserVisitor::class])]
 class Gpthub extends User
